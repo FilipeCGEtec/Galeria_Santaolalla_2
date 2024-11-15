1e0f647f69a6af0185d5b35929bd857c47ca9d82
YosagiGames <YosagiGamesCompany@gmail.com>
2024-11-14 23:05:40 -0300

    Update da pagina de biografia

---
+++ galeria_gs/lib/classes/Compositor.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Compositor{
+  String nome;
+  String biografia;
+  List<String> premios;
+  Compositor(this.nome, this.biografia, this.premios);
+
+  void TocarMusica(){
+
+  }
+
+  void MudarTempo(){
+
+  }
+}
---
+++ galeria_gs/lib/classes/Imagem.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Imagem{
+  String link;
+  double altura;
+  double largura;
+  Imagem(this.link, this.altura, this.largura);
+
+  void abrirImagem(){
+
+  }
+}
